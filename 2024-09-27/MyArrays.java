f60acc9ea1e2ddc04825880308c430a6bf112a56
Brian Zheng <brianz76@nycstudents.net>
2024-09-27 10:58:15 -0400

    Wrote a test case for returnCopy and started working on the function

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,4 +1,6 @@
 public class MyArrays {
+  public static int[] returnCopy(int[] )
+
   public static String aryToString(int[] nums) {
     String ans = "[";
     for (int i = 0; i< nums.length; i++) {
@@ -11,6 +13,12 @@ public class MyArrays {
     return ans;
   }
   public static void main(String[] args) {
+    // aryToString test
     System.out.println(aryToString(new int[] {1, 2, 3, 5, 6}));
+    // returnCopy test cases
+    int[] ary1 = {1, 2, 3, 6, 8, 3, 4};
+    System.out.println(aryToString(returnCopy(new int[] {1, 2, 3, 6, 8, 3, 4})).equals(aryToString(ary1)));
+
+
   }
 }
