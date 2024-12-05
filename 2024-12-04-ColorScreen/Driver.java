c1d146fbb4dd916b2987673debaa03344d2cc4bb
Brian Zheng <brianz76@nycstudents.net>
2024-12-05 07:50:50 -0500

    Added the rando nums and divider

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -22,19 +22,29 @@ public class Driver {
 
     public static int background(int color){
         return color + 10;
+
+    }
+    public static int bright(int color) {
+        return color + 60;
     }
 
     public static void goTo(int x, int y) {
         System.out.print(ESC + y + ";" + x + "H");
     }
 
-    public static void main(String[] args) {
-        System.out.print(CLEAR_SCREEN);
-
+    public static void numColor (int x) {
+        if (x < 25) {
+            color(bright(RED)); 
+        }
+        if (x > 75) {
+            color(bright(GREEN)); 
+        }
+    }
+    public static void background() {
         color(background(BLUE));
-        for (int x = 1; x <= 30; x++) {
-            if (x == 1 || x == 30) {
-                for (int y = 1; y <= 80; y++) {
+        for (int x = 1; x <= 80; x++) {
+            if (x == 1 || x == 80) {
+                for (int y = 1; y <= 30; y++) {
                     goTo(x, y);
                     System.out.print(" ");
                 }
@@ -42,12 +52,51 @@ public class Driver {
             else {
                 goTo(x, 1);
                 System.out.print(" ");
-                goTo(x, 80);
+                goTo(x, 30);
                 System.out.print(" ");
             }
         }
-
-        System.out.print(RESET);
     }
+
+    public static void drawRandoNums() {
+        int[] randomInts = new int[3];
+        for (int i = 0; i < 3; i++) {
+            int rand = (int) (Math.random() * 100);
+            randomInts[i] = rand;
+
+            goTo(1 + 20*(i+1), 2);
+            numColor(rand);
+            System.out.print(rand);
+            System.out.print(RESET);
+        }
+    }
+
+    public static void divider() {
+        for (int x = 2; x < 80; x++) {
+            goTo(x, 3);
+            System.out.print("=");
+        }
+    }
+    
+
+    public static void main(String[] args) {
+        System.out.print(CLEAR_SCREEN);
+
+        background();
+        
+        System.out.print(RESET);
+
+        drawRandoNums();
+
+        color(background(BLUE)); 
+
+        divider();
+
+        goTo(1, 31);
+        System.out.print(RESET);
+
+       
+    }
+
 
 }
