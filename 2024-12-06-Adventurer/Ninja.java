491c75d9dbad9e9bbd5c7e0fa82022a9674fbbd3
Brian Zheng <brianz76@nycstudents.net>
2024-12-06 10:33:22 -0500

    Added Adventurer and started on Ninja

---
+++ 2024-12-06-Adventurer/Ninja.java
@@ -0,0 +1,13 @@
+public class Ninja extends Adventurer {
+  private int stealth;
+
+  public Ninja(String name) {
+    super(name);
+  }
+
+  public Ninja(String name, int hp) {
+    super(name, hp);
+  }
+
+
+}
