91dc34f31fee9cfe7df532f28e32a0b88fb2c320
Brian Zheng <brianz76@nycstudents.net>
2024-12-06 10:58:33 -0500

    Started some heal/buff methods

--- 2024-12-06-Adventurer/Ninja.java
+++ 2024-12-06-Adventurer/Ninja.java
@@ -30,6 +30,35 @@ public class Ninja extends Adventurer {
     return stealthMax;
   }
 
+  public String attack(Adventurer other) {
+    other.applyDamage(10);
+    return other.getName() + " lost 10 HP!";
+  }
+
+  public String support(Adventurer other) {
+
+  }
+
+  public String support() {
+    if (getHP()+10 > getmaxHP()) {
+      return "HP buffed by " + (getmaxHP() - getHP()) + "to. Max health reached!"
+    }
+    setHP(getHP() + 10);
+    return
+  }
+
+  public String specialAttack(Adventurer other) {
+    int currentSpecial = getSpecial();
+    if (currentSpecial >= 10) {
+      setSpecial(currentSpecial - 10);
+
+    }
+    else {
+      System.out.println("not ")
+    }
+  }
+
+
 
 
 
