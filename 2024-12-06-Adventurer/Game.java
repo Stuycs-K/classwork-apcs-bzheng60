c65c24c058baf5130fb266f2a92d69014d5af7b4
Brian Zheng <brianz76@nycstudents.net>
2024-12-09 10:59:44 -0500

    Added user input and the corresponding action

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Game {
   public static void main(String[] args) {
     CodeWarrior c  = new CodeWarrior("Carmen", 100, "c++");
@@ -6,6 +8,35 @@ public class Game {
     while(true) {
       System.out.println(c.getName() + ", " + c.getHP() + "/" + c.getmaxHP() + " HP, " + c.getSpecial() + "/" + c.getSpecialMax() + " " + c.getSpecialName());
       System.out.println(n.getName() + ", " + n.getHP() + "/" + n.getmaxHP() + " HP, " + n.getSpecial() + "/" + n.getSpecialMax() + " " + n.getSpecialName());
+
+      Scanner input = new Scanner(System.in);
+      System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / (q)uit");
+      String action = input.nextLine();
+
+      if (action.equals("a")) {
+        c.attack(n);
+      }
+      else if (action.equals("sp")) {
+        c.specialAttack(n);
+      }
+      else if (action.equals("su")) {
+        c.support();
+      }
+      else if (action.equals("q")) {
+        System.out.println("Thanks for playing!");
+        return;
+      }
+      else {
+        System.out.println("Invaid input. Try again");
+      }
+
+      if (n.getHP() > 0) {
+        int choice = (int) (Math.random() * 4);
+        if (choice == 0) {
+          n.attack()
+        }
+      }
+
     }
   }
 }
